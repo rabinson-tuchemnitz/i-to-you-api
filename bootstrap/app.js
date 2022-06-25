29fb9d05128d4c5123cca260f82b63022d1d2817
Rabinson Ghatani <myself.ronin@hotmail.com>
2022-06-25 19:36:30 +0200

    adds helmet for securing apis

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -28,6 +28,8 @@ require('./passport')(passport)
 | Middlewares
 |--------------------------------------------------------------------------
 */
+app.use(require('helmet')())
+
 const bp = require('body-parser')
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
