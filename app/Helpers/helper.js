4bb289321d3d459e1a6bb6333f600a1399527440
Rabinson Ghatani <myself.ronin@hotmail.com>
2022-06-25 16:10:51 +0200

    fixes file download

--- app/Helpers/helper.js
+++ app/Helpers/helper.js
@@ -1,5 +1,7 @@
 module.exports = {
   generateDownloadUrl: (req, path) => {
-    return req.protocol + '://' + req.get('host') + '/api/download/' + path
+    return (
+      req.protocol + '://' + req.get('host') + '/api/files/download/' + path
+    )
   }
 }
