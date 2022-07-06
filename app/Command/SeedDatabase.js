d397e6283550c8f6baaf5af5a67d8fefafb084e0
Rabinson Ghatani <myself.ronin@hotmail.com>
2022-07-06 23:25:19 +0200

    fixes blocking requets

--- app/Command/SeedDatabase.js
+++ app/Command/SeedDatabase.js
@@ -1,25 +1,25 @@
-const RolesConstant = require('../Constants/RolesConstant')
-const User = require('../Models/User')
-const bcrypt = require('bcryptjs')
+const RolesConstant = require("../Constants/RolesConstant");
+const User = require("../Models/User");
+const bcrypt = require("bcryptjs");
 
 const seedUsersInDatabase = async () => {
-  await require('../../bootstrap/database')()
+  await require("../../bootstrap/database")();
 
-  const hashedPassword = await bcrypt.hash('password123', 15)
+  const hashedPassword = await bcrypt.hash("password123", 15);
 
-  console.log('Adding admin user')
+  console.log("Adding admin user");
   try {
     await User.create({
-      name: 'Admin',
-      email: 'admin@email.com',
+      name: "Admin",
+      email: "admin@email.com",
       password: hashedPassword,
       role: RolesConstant.ADMIN
-    })
-    console.log('Operation Completed')
+    });
+    console.log("Operation Completed");
   } catch (err) {
-    console.log(err)
-    console.log('Operation Failed')
+    console.log(err);
+    console.log("Operation Failed");
   }
-}
+};
 
-seedUsersInDatabase()
+return seedUsersInDatabase();
